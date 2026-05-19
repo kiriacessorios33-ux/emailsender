788bf18d94508af8f2246956c5819a716099f203
kiriacessorios33-ux <kiriacessorios33@gmail.com>
2026-05-18 21:19:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const { Resend } = require("resend");
 
 const app = express();
 
+app.use(express.json());
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 app.get("/", (req, res) => {
   res.send("Email sender online");
+});
+
+app.post("/send", async (req, res) => {
+  try {
+    const { to, subject, html } = req.body;
+
+    const data = await resend.emails.send({
+      from: "Suporte <onboarding@resend.dev>",
+      to,
+      subject,
+      html
+    });
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
